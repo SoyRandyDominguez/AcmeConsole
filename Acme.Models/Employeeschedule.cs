b3a898e619327eb87fb57285edcf983f08c652ea
Randy Dominguez <dominguezcalzado@gmail.com>
2021-12-11 17:59:50 -0400

    Ready

---
+++ Acme.Models/Employeeschedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Models
+{
+    public class EmployeeSchedule
+    {
+        public string Name { get; set; }
+        public List<string> Schedule { get; set; }
+        public EmployeeSchedule()
+        {
+            Schedule = new List<string>();
+        }
+    }
+}
---
+++ Acme.Models/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Models
+{
+    public class Schedule
+    {
+        public string Day { get; set; }
+        public string InitialHour { get; set; }
+        public string EndHour { get; set; }
+    }
+}
